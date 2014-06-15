3c38828cbbf8764e534c316f4745442d404614d1
aliabbasmd <aliabbasmd@yahoo.com>
2014-06-14 20:55:21 -0400

    updated working directory

--- Course project/run_analysis.R
+++ Course project/run_analysis.R
@@ -1,3 +1,5 @@
+#set working directory
+setwd("c:\\data")
 #download data file
 fileurl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileurl,"uhcdata.zip")
